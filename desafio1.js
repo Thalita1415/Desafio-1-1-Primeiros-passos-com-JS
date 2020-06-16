f3fafb2b14a07fcbec616720709a95f02e47fdb3
Thalita Freitas <thalita.freitas1415@hotmail.com>
2020-06-16 12:33:58 -0300

    primeiro commit

---
+++ desafio1.js
@@ -0,0 +1,28 @@
+const nome = "Thalita";
+const idade = 48;
+const sexo = "F ";
+const tempoC = 38;
+
+const somatempo = idade + tempoC
+console.log('O Tempo de Contribuição é: ' +somatempo)
+
+if(somatempo >= 85){
+    console.log('O contribuidor 1 pode se aposentar')
+}
+
+    else{
+    
+        console.log('O contribuidor 1 não pode se aposentar')
+    
+        }
+
+ if (somatempo >= 95){
+    console.log('O contribuidor 2 pode se aposentar')
+    
+} 
+
+  else{
+    console.log('O contribuidor 2 não pode se aposentar')
+
+        }
+    
